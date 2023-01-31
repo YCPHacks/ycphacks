cded385a27778b208fa18d5d1b26cdd90882a8b5
Ralph Greaves <rgreaves@ycp.edu>
2023-01-31 15:15:26 -0500

    Update index.mjs

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,6 +1,23 @@
 import express from 'express';
 
+import pkg from 'express-openid-connect';
+const { auth, requiresAuth } = pkg;
+
 const router = express.Router();
+
+router.use(auth({
+  authRequired: false,
+  authorizationParams: {
+    response_type: 'code',
+    scope: 'openid profile email'
+  }
+}));
+
+router.use((req, res, next) => {
+  res.locals.isAuthenticated = req.oidc.isAuthenticated();
+
+  next();
+});
 
 router.get('/', (req, res) => {
   res.status(200).render('index');
