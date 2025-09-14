4c2896f1aef1cb50ca8502eaeef23e6e84b9def8
Emily Culp <eculp@ycp.edu>
2025-09-14 17:50:58 -0400

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
@@ -6,18 +7,35 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
       crypto: 'crypto-browserify'
     }
   },
   optimizeDeps: {
     esbuildOptions: {
-      define: { global: 'globalThis' },
-      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })]
+      define: {
+        global: 'globalThis',
+        'process.env': '{}'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true
+        })
+      ]
     }
   },
   build: {
     rollupOptions: {
-      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })]
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true
+        })
+      ]
     }
+  },
+  server: {
+    port: 8080
   }
 })
