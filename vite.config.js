0bfb70ba14617048fe747a30d7a2d620d4f78845
Emily Culp <eculp@ycp.edu>
2025-09-14 17:40:05 -0400

    Crypto error still...

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
@@ -7,32 +6,18 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      crypto: 'crypto-browserify'
     }
   },
   optimizeDeps: {
     esbuildOptions: {
       define: { global: 'globalThis' },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-          crypto: true  // important for getRandomValues
-        })
-      ]
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })]
     }
   },
   build: {
-    target: 'esnext',
     rollupOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-          crypto: true
-        })
-      ]
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })]
     }
-  },
-  server: { port: 8080 }
+  }
 })
