97db810b2d615fcbaee625912f55be965c26de55
Emily Culp <eculp@ycp.edu>
2025-11-05 11:47:33 -0500

    #5: Trying to fix build test

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-icons/bootstrap-icons.css';
 
 import BootstrapVue3 from 'bootstrap-vue-3';
 
