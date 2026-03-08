4ac22cfcf0420a55092fe95215a60af5cfc73b42
Evan Natale <enatale1@ycp.edu>
2026-03-08 04:15:37 -0400

    Added an axios interceptor to add the Token automatically.

--- src/main.js
+++ src/main.js
@@ -10,8 +10,18 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import BootstrapVue3 from 'bootstrap-vue-3';
+import axios from "axios";
+
+// Axios interceptor to add the Token automatically
+axios.interceptors.request.use((config) => {
+    const cookie = document.cookie.split('; ').find(row => row.startsWith('token='));
+    if (cookie) {
+        const token = cookie.split('=')[1];
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
 
 const app = createApp(App);
 app.use(BootstrapVue3);
